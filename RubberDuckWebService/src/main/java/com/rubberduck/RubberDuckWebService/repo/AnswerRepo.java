dd7c35d602709392172dd1ea352b7b55582da6d2
Eric Ma <eric.m.990909@gmail.com>
2019-10-16 23:54:00 +0800

    add find by studentId and questionId

--- RubberDuckWebService/src/main/java/com/rubberduck/RubberDuckWebService/repo/AnswerRepo.java
+++ RubberDuckWebService/src/main/java/com/rubberduck/RubberDuckWebService/repo/AnswerRepo.java
@@ -3,9 +3,15 @@ package com.rubberduck.RubberDuckWebService.repo;
 import com.rubberduck.RubberDuckWebService.model.Answer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AnswerRepo extends JpaRepository<Answer, Integer> {
 
     Answer findById(Long id);
+
+    List<Answer> findByStudentId(Long studentId);
+
+    List<Answer> findByQuestionId(Long questionId);
 
     Answer save(Answer answer);
 
