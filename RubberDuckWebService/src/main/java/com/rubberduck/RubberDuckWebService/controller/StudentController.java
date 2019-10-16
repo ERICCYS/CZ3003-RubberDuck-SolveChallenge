f04229d4242f89bd1369432bb646251ce2e758c0
Eric Ma <eric.m.990909@gmail.com>
2019-10-16 13:49:53 +0800

    add more queries for student entity

--- RubberDuckWebService/src/main/java/com/rubberduck/RubberDuckWebService/controller/StudentController.java
+++ RubberDuckWebService/src/main/java/com/rubberduck/RubberDuckWebService/controller/StudentController.java
@@ -4,19 +4,43 @@ import com.rubberduck.RubberDuckWebService.JSONConvert;
 import com.rubberduck.RubberDuckWebService.model.Student;
 import com.rubberduck.RubberDuckWebService.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import java.security.NoSuchAlgorithmException;
 import java.util.List;
 
 @RestController
 public class StudentController {
+
     @Autowired
     StudentService studentService;
 
     @GetMapping("/students")
-    public String getAllAdmins() {
-        List<Student> admins = studentService.findAll();
-        return JSONConvert.JSONConverter(admins);
+    public String getAllStudents() {
+        List<Student> students = studentService.findAll();
+        return JSONConvert.JSONConverter(students);
+    }
+
+
+    @GetMapping("/student")
+    public String getStudentById(
+            @RequestParam Long id
+    ) {
+        Student student = studentService.findById(id);
+        return JSONConvert.JSONConverter(student);
+    }
+
+    @PostMapping("/customer")
+    @ResponseStatus(HttpStatus.CREATED)
+    public String createStudentAccount(
+            @Valid @RequestBody Student student
+    ) throws NoSuchAlgorithmException {
+        String hashedPassword = student.hashPassword(student.getPassword());
+        student.setPassword(hashedPassword);
+        JSONConvert.JSONConverter(studentService.save(student));
+//        return ValidationController.getAccessToken(student.getId(), "CUSTOMER");
+        return "OK";
     }
 }
