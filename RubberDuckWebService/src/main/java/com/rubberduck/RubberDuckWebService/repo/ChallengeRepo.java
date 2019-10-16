2ae7f3f0dc2cba4d57628d2a48ab9e87f0718b08
Eric Ma <eric.m.990909@gmail.com>
2019-10-17 00:12:45 +0800

    add find by creatorId

--- RubberDuckWebService/src/main/java/com/rubberduck/RubberDuckWebService/repo/ChallengeRepo.java
+++ RubberDuckWebService/src/main/java/com/rubberduck/RubberDuckWebService/repo/ChallengeRepo.java
@@ -3,9 +3,13 @@ package com.rubberduck.RubberDuckWebService.repo;
 import com.rubberduck.RubberDuckWebService.model.Challenge;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ChallengeRepo extends JpaRepository<Challenge, Integer> {
 
     Challenge findById(Long id);
+
+    List<Challenge> findByCreatorId(Long creatorId);
 
     Challenge save(Challenge challenge);
 
