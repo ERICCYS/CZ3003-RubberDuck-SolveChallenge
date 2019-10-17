b88bdd499fa4b8b1a1d63781d1349606405361da
Eric Ma <eric.m.990909@gmail.com>
2019-10-17 14:11:48 +0800

    add leader board service

---
+++ RubberDuckWebService/src/main/java/com/rubberduck/RubberDuckWebService/service/LeaderBoardService.java
@@ -0,0 +1,10 @@
+package com.rubberduck.RubberDuckWebService.service;
+
+import com.rubberduck.RubberDuckWebService.model.Student;
+
+import java.util.List;
+
+public interface LeaderBoardService {
+
+    List<Student> getLeaderBoard();
+}
