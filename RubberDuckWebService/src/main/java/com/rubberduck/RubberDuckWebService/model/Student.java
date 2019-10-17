8cbf07902f3e581f98fa9ee6a5fc1aefe950791d
Eric Ma <eric.m.990909@gmail.com>
2019-10-17 14:11:35 +0800

    add mark attribute

--- RubberDuckWebService/src/main/java/com/rubberduck/RubberDuckWebService/model/Student.java
+++ RubberDuckWebService/src/main/java/com/rubberduck/RubberDuckWebService/model/Student.java
@@ -1,18 +1,27 @@
 package com.rubberduck.RubberDuckWebService.model;
 
 import javax.persistence.*;
-import java.math.BigInteger;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 
 @Entity
 @Table(name = "Student")
 public class Student extends User {
 
+    @Column(name = "MARK", nullable = false)
+    private int mark;
+
     public Student() {
     }
 
-    public Student(String firstName, String lastName, String userName, String password) {
+    public Student(String firstName, String lastName, String userName, String password, int mark) {
         super(firstName, lastName, userName, password);
+        this.mark = mark;
+    }
+
+    public int getMark() {
+        return mark;
+    }
+
+    public void setMark(int mark) {
+        this.mark = mark;
     }
 }
