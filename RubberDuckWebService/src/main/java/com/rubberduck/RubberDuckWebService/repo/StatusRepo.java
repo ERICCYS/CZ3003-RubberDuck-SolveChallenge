def64975f36631b50ec33049df2891bfb7a46296
Eric Ma <eric.m.990909@gmail.com>
2019-10-17 16:02:49 +0800

    add status repo

---
+++ RubberDuckWebService/src/main/java/com/rubberduck/RubberDuckWebService/repo/StatusRepo.java
@@ -0,0 +1,17 @@
+package com.rubberduck.RubberDuckWebService.repo;
+
+import com.rubberduck.RubberDuckWebService.model.Status;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface StatusRepo extends JpaRepository<Status, Integer> {
+
+    List<Status> findByStudentId(Long studentId);
+
+    List<Status> findByStudentIdAndCharacter(Long studentId, String character);
+
+    Status save(Status status);
+
+    void delete(Status status);
+}
