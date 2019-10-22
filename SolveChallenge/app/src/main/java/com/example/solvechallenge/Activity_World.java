f64d3822057490e2bd51aebf4feb0634aeef3097
Yuhao <heyu0012@ntu.edu.sg>
2019-10-22 17:21:32 +0800

    still contructing questions part

--- SolveChallenge/app/src/main/java/com/example/solvechallenge/Activity_World.java
+++ SolveChallenge/app/src/main/java/com/example/solvechallenge/Activity_World.java
@@ -5,10 +5,16 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class Activity_World extends AppCompatActivity {
 
-    private Button enter_intro_btn;
+    private Button enter_intro_btn = (Button) findViewById(R.id.btn_intro_World);;
+    private Button enter_analysis_btn = (Button) findViewById(R.id.btn_analysis_World);
+    private Button enter_design_btn = (Button) findViewById(R.id.btn_design_World);
+    private Button enter_testing_btn = (Button) findViewById(R.id.btn_testing_World);
+    private Button enter_maintainance_btn = (Button) findViewById(R.id.btn_maintainance_World);
+
 
     public void switchToNextActivity(View view) {
         Intent intent = new Intent(this, Activity_Section.class);
@@ -21,16 +27,56 @@ public class Activity_World extends AppCompatActivity {
         setContentView(R.layout.activity_world);
         this.getSupportActionBar().hide();
 
-        enter_intro_btn = (Button) findViewById(R.id.btn_intro_World); // type name Activity_name
+
+
+
+        // todo get
+
+
+
+
 
         enter_intro_btn.setOnClickListener(new View.OnClickListener(){
-
             public void onClick(View v){
 
-                switchToNextActivity(v);
+                if (0<= world_upperbound){
+                    switchToNextActivity(v);
+                    App_Data.setWorld(0);
+                }
+
+                else{
+                    Toast.makeText(Activity_World.this, "it is locked", Toast.LENGTH_SHORT).show();
+                }
 
             }
+        });
 
+        enter_analysis_btn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                switchToNextActivity(v);
+                App_Data.setWorld("analysis");
+            }
+        });
+
+        enter_design_btn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                switchToNextActivity(v);
+                App_Data.setWorld("design");
+            }
+        });
+
+        enter_testing_btn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                switchToNextActivity(v);
+                App_Data.setWorld("testing");
+            }
+        });
+
+        enter_maintainance_btn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                switchToNextActivity(v);
+                App_Data.setWorld("maintenance");
+            }
         });
 
     }
---
+++ SolveChallenge/app/src/main/java/com/example/solvechallenge/App_Data.java
@@ -0,0 +1,97 @@
+package com.example.solvechallenge;
+
+public class App_Data {
+
+
+
+    public static String userId;
+    public static String accessToken;
+
+    public static String character;
+    public static int world;
+    public static int section;
+    public static int level;
+
+    public static int no_question;
+
+    public static int world_upperbound;
+    public static int section_upperbound;
+    public static int level_upperbound;
+
+
+    public static String getAccessToken() {
+        return accessToken;
+    }
+
+    public static String getBaseUrl() {
+        return baseUrl;
+    }
+
+    public static String getUserId() {
+        return userId;
+    }
+
+    public static String getCharacter() {
+        return character;
+    }
+
+    public static int getWorld() {
+        return world;
+    }
+
+    public static int getSection() {
+        return section;
+    }
+
+    public static int getLevel() {
+        return level;
+    }
+
+    public static int getNo_question() {
+        return no_question;
+    }
+
+    public static int getWorld_upperbound() {
+        return world_upperbound;
+    }
+
+    public static int getSection_upperbound() {
+        return section_upperbound;
+    }
+
+    public static int getLevel_upperbound() {
+        return level_upperbound;
+    }
+
+    public static void setAccessToken(String accessToken) {
+        App_Data.accessToken = accessToken;
+    }
+
+    public static void setCharacter(String character) {
+        App_Data.character = character;
+    }
+
+    public static void setWorld(int world) {
+        App_Data.world = world;
+    }
+
+    public static void setSection(int section) {
+        App_Data.section = section;
+    }
+
+    public static void setLevel(int level) {
+        App_Data.level = level;
+    }
+
+    public static void setNo_question(int no_question) {
+        App_Data.no_question = no_question;
+    }
+
+    public static void setUserId(String userId) { App_Data.userId = userId; }
+
+    public static void setWorld_upperbound(int world_upperbound) { App_Data.world_upperbound = world_upperbound; }
+
+    public static void setSection_upperbound(int section_upperbound) { App_Data.section_upperbound = section_upperbound; }
+
+    public static void setLevel_upperbound(int level_upperbound) { App_Data.level_upperbound = level_upperbound; }
+}
