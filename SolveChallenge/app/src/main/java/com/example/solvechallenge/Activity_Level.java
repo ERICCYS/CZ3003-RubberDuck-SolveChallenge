6c489ce1d8155bfd563f5474191b77522757d5b3
Yuhao <heyu0012@ntu.edu.sg>
2019-10-22 11:09:38 +0800

    constructing the world and level page

--- SolveChallenge/app/src/main/java/com/example/solvechallenge/Activity_Level.java
+++ SolveChallenge/app/src/main/java/com/example/solvechallenge/Activity_Level.java
@@ -4,11 +4,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageButton;
 
 public class Activity_Level extends AppCompatActivity {
 
-    private Button start_question_btn;
+    private ImageButton start_easy_question_btn;
+    private ImageButton start_mid_question_btn;
+    private ImageButton start_hard_question_btn;
 
     public void switchToNextActivity(View view) {
         Intent intent = new Intent(this, Activity_Questions.class);
@@ -21,9 +23,12 @@ public class Activity_Level extends AppCompatActivity {
         setContentView(R.layout.activity_level);
         this.getSupportActionBar().hide();
 
-        start_question_btn = (Button) findViewById(R.id.btn_startquestions_Level); // type name Activity_name
+        start_easy_question_btn = (ImageButton) findViewById(R.id.imgbtn_easylevel_Level); // type name Activity_name
+        start_mid_question_btn = (ImageButton) findViewById(R.id.imgbtn_midlevel_Level);
+        start_hard_question_btn = (ImageButton) findViewById(R.id.imgbtn_hardlevel_Level);
 
-        start_question_btn.setOnClickListener(new View.OnClickListener(){
+
+        start_easy_question_btn.setOnClickListener(new View.OnClickListener(){
 
             public void onClick(View v){
 
