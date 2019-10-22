3a38287c00e611b49ade774f45b8c46efcdbef99
Eric Ma <eric.m.990909@gmail.com>
2019-10-22 20:51:16 +0800

    refactor constant data

--- RubberDuckWebService/src/main/java/com/rubberduck/RubberDuckWebService/service/StatusServiceImpl.java
+++ RubberDuckWebService/src/main/java/com/rubberduck/RubberDuckWebService/service/StatusServiceImpl.java
@@ -13,48 +13,48 @@ import java.util.Map;
 @Service
 public class StatusServiceImpl implements StatusService {
 
-    private Map<String, Integer> worldCodes = new HashMap<>();
-
-    private List<Map<String, Integer>> sectionCodes = new ArrayList<>();
-
-    private void setWorldCodes() {
-        this.worldCodes.put("Requirement Gathering and Analysis", 0);
-        this.worldCodes.put("Design", 1);
-        this.worldCodes.put("Implementation", 2);
-        this.worldCodes.put("Testing and Deployment", 3);
-        this.worldCodes.put("Maintenance", 4);
-    }
-
-    private void setSectionCodes() {
-        this.sectionCodes.add(new HashMap<String, Integer>() {{
-            put("Requirement Engineering", 0);
-            put("Requirement Analysis", 1);
-            put("Requirement Specification and Validations", 2);
-        }});
-        this.sectionCodes.add(new HashMap<String, Integer>() {{
-            put("Design Phrase I", 0);
-            put("Design Phrase II", 1);
-            put("Design Phrase III", 2);
-        }});
-        this.sectionCodes.add(new HashMap<String, Integer>() {{
-            put("Implementation FE Dev", 0);
-            put("Implementation BE Dev", 1);
-            put("Implementation ALL", 2);
-        }});
-        this.sectionCodes.add(new HashMap<String, Integer>() {{
-            put("Testing", 0);
-            put("Deployment", 1);
-            put("DevOps", 2);
-        }});
-        this.sectionCodes.add(new HashMap<String, Integer>() {{
-            put("Maintenance I", 0);
-            put("Maintenance II", 1);
-            put("Maintenance III", 2);
-        }});
-    }
-
     @Autowired
     StatusRepo statusRepo;
+
+    public static final Map<String, Integer> worldCodes = new HashMap<String, Integer>(){
+        {
+            put("Requirement Gathering and Analysis", 0);
+            put("Design", 1);
+            put("Implementation", 2);
+            put("Testing and Deployment", 3);
+            put("Maintenance", 4);
+        }
+    };
+
+    public static final List<Map<String, Integer>> sectionCodes = new ArrayList<Map<String, Integer>>(){
+        {
+            add(new HashMap<String, Integer>() {{
+                put("Requirement Engineering", 0);
+                put("Requirement Analysis", 1);
+                put("Requirement Specification and Validations", 2);
+            }});
+            add(new HashMap<String, Integer>() {{
+                put("Design Phrase I", 0);
+                put("Design Phrase II", 1);
+                put("Design Phrase III", 2);
+            }});
+            add(new HashMap<String, Integer>() {{
+                put("Implementation FE Dev", 0);
+                put("Implementation BE Dev", 1);
+                put("Implementation ALL", 2);
+            }});
+            add(new HashMap<String, Integer>() {{
+                put("Testing", 0);
+                put("Deployment", 1);
+                put("DevOps", 2);
+            }});
+            add(new HashMap<String, Integer>() {{
+                put("Maintenance I", 0);
+                put("Maintenance II", 1);
+                put("Maintenance III", 2);
+            }});
+        }
+    };
 
     @Override
     public List<Status> findByStudentId(Long studentId) {
@@ -63,8 +63,6 @@ public class StatusServiceImpl implements StatusService {
 
     @Override
     public Status findByStudentIdAndCharacter(Long studentId, String character) {
-        this.setWorldCodes();
-        this.setSectionCodes();
         Status status = statusRepo.findByStudentIdAndCharacter(studentId, character);
         Integer worldCode = worldCodes.get(status.getWorld());
         status.setWorld(worldCode.toString());
