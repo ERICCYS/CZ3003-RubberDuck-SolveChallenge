9ecde185883941a59fa94d6d8f3e0cc3c41fdcfe
Yuhao <heyu0012@ntu.edu.sg>
2019-10-22 14:04:17 +0800

    add chenchen's login part

--- SolveChallenge/app/src/main/java/com/example/solvechallenge/Activity_Questions.java
+++ SolveChallenge/app/src/main/java/com/example/solvechallenge/Activity_Questions.java
@@ -2,13 +2,30 @@ package com.example.solvechallenge;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Activity_Questions extends AppCompatActivity {
+
+    TextView tv_question = (TextView) findViewById(R.id.tv_question_Questions);
+    Button brn_choice1 = (Button)findViewById(R.id.btn_choice1_Questions);
+    Button btn_choice2 = (Button)findViewById(R.id.btn_choice2_Questions);
+    Button btn_choice3 = (Button)findViewById(R.id.btn_choice3_Questions);
+    Button btn_choice4 = (Button)findViewById(R.id.btn_choice4_Questions);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_questions);
         this.getSupportActionBar().hide();
+
+
+//        quenstion_answer_data = {}; // json
+
+
+
     }
+
+
 }
+
--- SolveChallenge/app/src/main/java/com/example/solvechallenge/Activity_SignIn.java
+++ SolveChallenge/app/src/main/java/com/example/solvechallenge/Activity_SignIn.java
@@ -1,14 +1,24 @@
 package com.example.solvechallenge;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.InputType;
+import android.text.method.PasswordTransformationMethod;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class Activity_SignIn extends AppCompatActivity {
 
-    private  Button sign_in_btn;
+    EditText name_box, email_box, password_box;
+    Button password_show, start_button;
+    Spinner user_role_spinner;
+    String user_role;
 
     public void switchToNextActivity(View view) {
         Intent intent = new Intent(this, Activity_SelectCharacter.class);
@@ -21,19 +31,86 @@ public class Activity_SignIn extends AppCompatActivity {
         setContentView(R.layout.activity_sign_in);
         this.getSupportActionBar().hide();
 
+        name_box = findViewById(R.id.name_box);
+        email_box = findViewById(R.id.email_box);
+        password_box = (EditText) findViewById(R.id.password_box);
+        password_show.setOnClickListener(new showButtonListener());
 
-        sign_in_btn = (Button) findViewById(R.id.btn_sign_in_SignIn); // type name Activity_name
+        //set up user role spinner
+        user_role_spinner = (Spinner) findViewById(R.id.user_role_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.user_roles, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        user_role_spinner.setAdapter(adapter);
+        user_role_spinner.setOnItemSelectedListener(new spinnerListener());
 
-        sign_in_btn.setOnClickListener(new View.OnClickListener(){
+        //Once the user enter the game, validate user info
+        start_button = (Button) findViewById(R.id.start_button);
+        start_button.setOnClickListener(new View.OnClickListener(){
 
-            public void onClick(View v){
+            @Override
+            public void onClick(View v) {
+                String name = name_box.getText().toString();
+                String email = email_box.getText().toString();
+                String password = password_box.getText().toString();
 
-                switchToNextActivity(v);
-
+                if(name.equals("") || email.equals("") || password.equals("")){
+                    Toast.makeText(LoginPage.this, "Please Enter the Details", Toast.LENGTH_SHORT).show();
+                }else{
+                    boolean login_successful = findUser(name, email, password);
+                    if(login_successful){
+                        if(user_role.equals("Staff")){
+                            // start next staff activity
+                            //TODO
+                        }else if(user_role.equals("Student")){
+                            //start next student activity
+                            //TODO
+                        }
+                    }else{
+                        Toast.makeText(LoginPage.this, "Sorry, user not found.", Toast.LENGTH_SHORT).show();
+                    }
+                }
             }
-
         });
+
 
     }
 
+    public boolean findUser(String name, String email, String password){
+        // consult backend
+        //TODO
+        return true;
+    }
+
+    class showButtonListener implements View.OnClickListener{
+
+        @Override
+        public void onClick(View v) {
+            if(password_show.getText().toString().equals("SHOW")){
+                password_box.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                password_box.setText("HIDE");
+            }else{
+                password_box.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                password_show.setText("SHOW");
+            }
+        }
+    }
+
+    class spinnerListener implements AdapterView.OnItemSelectedListener{
+
+
+        @Override
+        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            user_role = parent.getItemAtPosition(position).toString();
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> parent) {
+            Toast.makeText(getApplicationContext(), "Please select your role.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
 }
+
+
+
+
