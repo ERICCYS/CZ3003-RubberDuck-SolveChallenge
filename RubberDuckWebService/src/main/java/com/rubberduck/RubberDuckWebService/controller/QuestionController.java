7bc00be2d3c68c78e5a808067e7a63dd91dbc549
Eric Ma <eric.m.990909@gmail.com>
2019-11-05 23:09:30 +0800

    add get questions by ids

--- RubberDuckWebService/src/main/java/com/rubberduck/RubberDuckWebService/controller/QuestionController.java
+++ RubberDuckWebService/src/main/java/com/rubberduck/RubberDuckWebService/controller/QuestionController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -20,9 +21,14 @@ public class QuestionController {
     @Autowired
     ValidationService validationService;
 
-    @GetMapping("/questions")
-    public String getAllQuestion() {
-        List<Question> questions = questionService.findAll();
+    @PostMapping("/questions")
+    public String getQuestionsById(
+            @RequestBody List<Long> questionIds
+    ) {
+        List<Question> questions = new ArrayList<Question>();
+        for (Long questionId : questionIds) {
+            questions.add(questionService.findById(questionId));
+        }
         return JSONConvert.JSONConverter(questions);
     }
 
